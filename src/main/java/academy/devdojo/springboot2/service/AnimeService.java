4c6ea55d0466b5ff6bb4be9cc73247f0d6431c39
Danilzs <danzone00@gmail.com>
2025-08-31 18:32:06 -0300

    Springboot 8

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,16 +1,27 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
-    public List<Anime> ListAll(){
 
-        return List.of(new Anime(1L, "One piece"), new Anime(2L, "Naruto"));
+    private  List<Anime> animes = List.of(new Anime(1L, "One piece"), new Anime(2L, "Naruto"));
+    public List<Anime> listAll(){
+
+        return animes;
     }
 
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
 }
