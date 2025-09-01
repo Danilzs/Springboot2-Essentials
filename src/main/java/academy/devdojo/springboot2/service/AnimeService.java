7e600ac75c3939ef3a3db430c46933d600cba259
Danilzs <danzone00@gmail.com>
2025-09-01 11:28:56 -0300

    Springboot 9 - Post mapping

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,13 +6,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
 
-    private  List<Anime> animes = List.of(new Anime(1L, "One piece"), new Anime(2L, "Naruto"));
+    private static List<Anime> animes;
+
+    static{
+        animes = new ArrayList<>(List.of(new Anime(1L, "One piece"), new Anime(2L, "Naruto")));
+    }
+
     public List<Anime> listAll(){
 
         return animes;
@@ -24,4 +31,10 @@ public class AnimeService {
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
+    public Anime save(Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
+    }
+
 }
