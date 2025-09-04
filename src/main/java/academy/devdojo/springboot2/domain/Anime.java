12c984011aca4bb9adb32e662be2cca628ddc17b
Danilzs <danzone00@gmail.com>
2025-09-03 22:00:17 -0300

    Springboot 13 - Spring data

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,12 +1,21 @@
 package academy.devdojo.springboot2.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 //Representa o que temos como banco de dados.
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
